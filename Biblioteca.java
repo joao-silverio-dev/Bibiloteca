980aa8bd2d4bc173d5251ffb4b4b4339f775baf3
João Silvério <52933126+joao-silverio-dev@users.noreply.github.com>
2025-08-22 12:43:40 -0300

    logica biblioteca implementada ainda com erro

--- Biblioteca.java
+++ Biblioteca.java
@@ -6,32 +6,37 @@ public class Biblioteca {
     Scanner consoleRead = new Scanner(System.in);
 
     public Biblioteca(){
+        System.out.println("Bem vindo ao sistema da biblioteca\n");
         itensBiblioteca = new ArrayList<>();
     }
 
     public void adicionarItem(){
-        System.out.println("Digite o nome do autor ou publicadora: ");
+        System.out.print("\nDigite o nome do autor ou publicadora: ");
         String nomeAutor = consoleRead.nextLine();
-        System.out.println("Digite o nome do livro ou revista: ");
+        System.out.print("\nDigite o nome do livro ou revista: ");
         String nomeLivro = consoleRead.nextLine();
-        System.out.println("Digite o ano de publicação: ");
+        System.out.print("\nDigite o ano de publicação: ");
         int anoPubli = consoleRead.nextInt();
+        consoleRead.nextLine();
 
         Autor autor = new Autor(nomeAutor);
 
-        System.out.println("Qual item deseja adicionar? \n(1) Livro\n(2) Revista\n\nItem: ");
+        System.out.println("\n\nQual item deseja adicionar? \n\t(1) Livro\n\t(2) Revista\n\nItem: ");
         int numero = consoleRead.nextInt();
+        consoleRead.nextLine();
 
         if(numero == 1){
             itensBiblioteca.add(new Livro(nomeLivro, autor, anoPubli));
+            System.out.println("Número de itens atualizado: " + itensBiblioteca.size());
         } else{
             itensBiblioteca.add(new Revista(nomeAutor, autor, anoPubli));
+            System.out.println("Número de itens atualizado: " + itensBiblioteca.size());
         }
     }
 
     public void buscaPorTitulo(String titulo){
         for (ItemBiblioteca itemBibObject : itensBiblioteca) {
-            if(itemBibObject.titulo == titulo){
+            if(itemBibObject.titulo.equals(titulo)){
                 System.out.println("Item encontrado");
                 System.out.println("\tTítulo: " + itemBibObject.titulo + "\n" +
                                    "\tNome do Autor: " + itemBibObject.autor.nome + "\n" +
--- Main.java
+++ Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        Scanner consoleRead = new Scanner(System.in);
+        int loop;
+
+        Biblioteca bibliotecaTeste = new Biblioteca();
+        do {
+            System.out.println("\nQual operação deseja realizar?");
+            System.out.println("\t0 - Sair");
+            System.out.println("\t1 - Adicionar item");
+            System.out.println("\t2 - Busca por título");
+            System.out.println("\t3 - Listar todos os itens");
+            loop = consoleRead.nextInt();
+            consoleRead.nextLine();
+
+            switch (loop) {
+                case 1:
+                    bibliotecaTeste.adicionarItem();
+                    break;
+                case 2:
+                    System.out.print("Digite o título a ser procurado (lembre-se dos caracteres maiúsculos antes de cada palavra e acentuações).\n\nTítulo: ");
+                    String titulo = consoleRead.nextLine();
+                    consoleRead.nextLine();
+                    bibliotecaTeste.buscaPorTitulo(titulo);
+                    break;
+                case 3:
+                    bibliotecaTeste.listarTodosOsItens();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Não entendi a operação, tente novamente.");
+                    break;
+            }
+        } while (loop != 0);
+
     }
 }
